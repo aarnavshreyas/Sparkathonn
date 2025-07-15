9351542f18082a38e04fef0c3c64afc869bba11e
aarnavshreyas <aarnavshreyas2005@gmail.com>
2025-07-15 14:59:22 +0530

    fixed the background

--- components/ui/demo.tsx
+++ components/ui/demo.tsx
@@ -1,5 +1,5 @@
 import { BackgroundPaths } from "@/components/ui/background-paths"
 
 export function DemoBackgroundPaths() {
-    return <BackgroundPaths onlyBackground />
+    return <BackgroundPaths />
 } 
